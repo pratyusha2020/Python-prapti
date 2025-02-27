50452cbfbc32306ebda4bbbf1a30e28fb04c9946
= <=>
2025-02-26 22:00:19 -0800

    g

---
+++ Day02_homework.py
@@ -0,0 +1,11 @@
+Birthday01='25/2/2011'
+Birthday02='29/1/2013'
+Birthday03='15/11/2013'
+Birthday04='16/1/1998'
+Birthday05='24/8/2022'
+print("Prapti's birthday is on",Birthday01)
+print("\nRocky's birthday is on" ,Birthday02)
+print("\n Navya's birthday is on",Birthday03)
+print("\n Tam's birthday is on",Birthday04)
+print("\n Arna's birthday is on",Birthday05)
+#I made the birthdays strings because otherwise it was doing division, and converting the birthdays into decimals.
