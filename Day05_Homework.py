6096f54d2a794415a440cc630f172243363e6646
= <=>
2025-03-09 12:05:04 -0700

    n

---
+++ Day05_Homework.py
@@ -0,0 +1,3 @@
+number=float(input("square?"))
+squareroot=number**0.5
+print(squareroot)
---
+++ Day5_PythonOperators1.py
@@ -0,0 +1,14 @@
+# tree1=98
+# tree2=94
+# tree3=41
+# tree4=95
+# tree5=11
+# sum=tree1+tree2+tree3+tree4+tree5
+# average=sum/5
+# print(average)
+math=float(input("marks in math"))
+science=float(input("marks in science"))
+english=float(input("marks in english"))
+sum=math+science+english
+percentage=(sum/300)*100
+print(percentage)
