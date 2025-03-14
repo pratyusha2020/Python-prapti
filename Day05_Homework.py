d7d28dd0f1b3fa45a854faf4380b2ea8d48af801
= <=>
2025-03-13 21:12:39 -0700

    v

--- Day05_Homework.py
+++ Day05_Homework.py
@@ -1,3 +1,5 @@
-number=float(input("square?"))
-squareroot=number**0.5
-print(squareroot)
+temp=float(input("Please enter today's temperature in Celsuis"))
+if (temp>=15): 
+    print("You may wear light clothing today")
+else:
+    print("You should wear warm clothing today")
---
+++ Day_05Indentation.py
@@ -0,0 +1,5 @@
+age=int(input("Please Enter your Age"))
+if age>=18:
+    print("You can vote")
+else:
+    print("You cannot vote")
