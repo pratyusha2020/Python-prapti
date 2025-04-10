24e6ed07c523c4ec433e5cfe1472c4385028444f
= <=>
2025-04-09 22:25:37 -0700

    v

---
+++ Python Codes/Loops/ACT1.py
@@ -0,0 +1,7 @@
+fruits= ["Banana","Peach","Mango"]
+for fruit in fruits:
+    print(fruit)
+
+Variable= "Tortoise"
+for i in Variable:
+    print(i)
---
+++ Python Codes/Loops/ACT3.py
@@ -0,0 +1,6 @@
+string= input("Enter a string:")
+string2=('')
+for i in string:
+    string2=i+string2
+print ("Original String=",string)
+print ("Reversed String=",string2)
